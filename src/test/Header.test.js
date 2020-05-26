88fd3923dd5ff902736741c6bbedb67e31ac8681
smanoharan <smanoharan@empoweredbenefits.com>
2020-05-26 17:09:36 +0530

    login implemented

---
+++ src/test/Header.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import Header from '../modules/header/components/Header';
+
+describe('Header tests', () => {
+  it('renders Header', () => {
+    const wrapper = shallow(<Header />);
+
+    // Expect the wrapper object to be defined
+    // expect(wrapper.find('.list-items')).toBeDefined();
+  });
+});
