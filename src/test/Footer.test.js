1d6d736ee989825eda7dba4cd0f27059b33e09b9
smanoharan <smanoharan@empoweredbenefits.com>
2020-05-27 18:11:55 +0530

    changes on 27/20

--- src/test/Footer.test.js
+++ src/test/Footer.test.js
@@ -6,8 +6,5 @@ import Footer from '../modules/common/components/Footer';
 describe('Footer tests', () => {
   it('renders Footer', () => {
     shallow(<Footer />);
-
-      // Expect the wrapper object to be defined
-      // expect(wrapper.find('.list-items')).toBeDefined();
   });
 });
--- src/test/Header.test.js
+++ src/test/Header.test.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-
+import configureMockStore from 'redux-mock-store';
 import Header from '../modules/header/components/Header';
+import { shallowToJson } from 'enzyme-to-json';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import reducers from '../store';
+import { Provider } from 'react-redux';
+import { mount } from 'enzyme';
+import { MemoryRouter } from 'react-router';
 
-describe('Header tests', () => {
-  it('renders Header', () => {
-    const wrapper = shallow(<Header />);
+const store = createStore(reducers, applyMiddleware(thunk));
 
-    // Expect the wrapper object to be defined
-    expect(wrapper.find('.list-items')).toBeDefined();
-  });
+describe('header Component', () => {
+  const output = shallow(<Header />);
+  expect(shallowToJson(output)).toMatchSnapshot();
 });
